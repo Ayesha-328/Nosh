fe4f9950f8584ea0f6c2613c957f06d255bb1a07
Ayesha Najeeb <ayeshanajeeb2803@gmail.com>
2024-06-05 23:44:16 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
+    <Route path="/" element={<Home/>
+    }  />
     <Route path="/about" element={<About/>
     }  />
     <Route path="/menu" element={<Menu/>
